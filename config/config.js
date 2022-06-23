edac9a4d435453076bf7882bb1cea00970d7cf8b
coder71 <kamrunnaher877@gmail.com>
2022-06-23 20:37:30 +0600

    app initial structure

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const config = {
+  PORT: process.env.PORT,
+  MONGODB_URI: process.env.MONGODB_URI,
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const http = require('http');
+const cors = require('cors');
+const config = require('./config/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// midlewares
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('connection successfull'))
+  .catch((err) => console.log(err));
+
+// initiate the server
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+// app listner
+server.listen(config.PORT, () => {
+  console.log('server is running in localhost:', config.PORT);
+});
